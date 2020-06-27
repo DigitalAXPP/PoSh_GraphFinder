72667f3bc285ce8e383215c461b5880fbb831380
DigitalAXPP <a.piepenhagen@gmx.net>
2020-06-28 00:04:11 +0200

    Completing the Node class. Throws error though.

--- Node.ps1
+++ Node.ps1
@@ -1,12 +1,31 @@
 class Node {
     [string]$NodeName
     [System.Collections.ArrayList]$Edge
+    [System.Collections.ArrayList]$Traversed
+    [System.Collections.Queue]$Queue
 
     Node([string]$Name) {
         $this.NodeName = $Name
     }
 
-    [Node]Add_Edge($NewEdge){
-        return $this.Edge.Add($NewEdge)
+    [Node]Add_Edge([Node]$NewEdge){
+        return $this.Edge.Add($NewEdge.NodeName)
+    }
+
+    [Node]Traverse(){
+        foreach ($item in $this.Edge) {
+            $this.Queue.Enqueue($item)
+        }
+
+        while (-not($this.Queue.Count -eq 0)) {
+            [Node]$vertex = $this.Queue.Dequeue()
+            if ($this.Traversed -notcontains $vertex) {
+                $this.Traversed.Add($vertex)
+                foreach ($item in $vertex.Edge) {
+                    $this.Queue.Enqueue($item)
+                }
+            }
+        }
+        return $this
     }
 }
