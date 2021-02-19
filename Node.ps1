af914cd46489d85981f15b31bddae2775b865158
Alex <Alex.Piepen@gmail.com>
2021-02-20 00:18:24 +0100

    Foundation (#2)
    * Creating base files for the project.
    
    * Using Stack for traversal.
    
    * Creating a new class 'Node'.
    
    * Completing the Node class. Throws error though.
    
    * Fixing the Node class.
    
    * Adding Breadth-first search.
    
    * Adding new 'ShortestPath' to try to find the shortest path in a graph.

--- Node.ps1
+++ Node.ps1
@@ -3,12 +3,21 @@ class Node {
     [System.Collections.ArrayList]$Edge
     [System.Collections.ArrayList]$Traversed
     [System.Collections.Queue]$Queue
+    [System.Collections.ArrayList]$Path
 
     Node([string]$Name) {
         $this.NodeName = $Name
         $this.Edge = New-Object -TypeName System.Collections.ArrayList        
         $this.Traversed = New-Object -TypeName System.Collections.ArrayList
         $this.Queue = New-Object -TypeName System.Collections.Queue
+        $this.Path = New-Object -TypeName System.Collections.ArrayList
+    }
+
+    [Node]Add_Edge([Node]$NewEdge) {
+        return $this.Edge.Add($NewEdge)
+    }
+
+    [Node]TraverseDepthFirst() {
     }
 
     [Node]Add_Edge([Node]$NewEdge){
@@ -30,5 +39,48 @@ class Node {
             }
         }
         return $this
+    }
+
+    [Node]TraverseBreadthFirst() {
+        foreach ($item in $this.Edge) {
+            $this.Queue.Enqueue($item)
+        }
+        
+        $this.Traversed.Add($this)
+        while (-not($this.Queue.Count -eq 0)) {
+            [Node]$vertex = $this.Queue.Dequeue()
+            if ($this.Traversed -notcontains $vertex) {
+                $this.Traversed.Add($vertex)
+                foreach ($item in $vertex.Edge) {
+                    $this.Queue.Enqueue($item)
+                }
+            }
+        }
+        return $this
+    }
+
+    [Node]ShortestPath() {
+        foreach ($item in $this.Edge) {
+            $this.Queue.Enqueue($item)
+        }
+
+        while (-not($this.Queue.Count -eq 0)) {
+            [Node]$vertex = $this.Queue.Dequeue()
+            if ($this.Traversed -notcontains $vertex) {
+                $this.Traversed.Add($vertex)
+                $this.Path.Add($vertex)
+                if ($vertex.NodeName -eq "D") {
+                    Write-Verbose -Verbose "Shortest path is here."
+                    break;
+                }
+                elseif ($vertex.Edge.Count -eq 0) {
+                    $this.Path.Remove($vertex)
+                }
+                foreach ($item in $vertex.Edge) {
+                    $this.Queue.Enqueue($item)
+                }
+            }
+        }
+        return $this;
     }
 }
